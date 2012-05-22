78d2387f7b0f6d8f66e882d85ff461408b52c312
Sean Upton <sdupton@gmail.com>
2012-05-22 14:27:56 -0600

    use aq_base() on object in defensive handler check; uses conditional import to avoid hard dependency on Zope2

--- plone/uuid/handlers.py
+++ plone/uuid/handlers.py
@@ -9,12 +9,17 @@ from plone.uuid.interfaces import IAttributeUUID
 
 from plone.uuid.interfaces import ATTRIBUTE_NAME
 
+try:
+    from Acquisition import aq_base
+except ImportError:
+    aq_base = lambda v: v  # soft-dependency on Zope2, fallback
+
 
 @adapter(IAttributeUUID, IObjectCreatedEvent)
 def addAttributeUUID(obj, event):
     
     if not IObjectCopiedEvent.providedBy(event):
-        if getattr(obj, ATTRIBUTE_NAME, None):
+        if getattr(aq_base(obj), ATTRIBUTE_NAME, None):
             return  # defensive: keep existing UUID on non-copy create
     
     generator = queryUtility(IUUIDGenerator)
