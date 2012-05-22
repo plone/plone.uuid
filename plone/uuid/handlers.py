c6dfdf8747913de9006afece46a92be45b68316c
Sean Upton <sdupton@gmail.com>
2012-05-22 13:58:31 -0600

    defensive UUID assignment on handler: do not create UUID twice, overwriting a previous value, except on IObjectCopiedEvent, where we do want to overwrite the stored UUID value, as a copy needs a distinct UUID.

--- plone/uuid/handlers.py
+++ plone/uuid/handlers.py
@@ -2,6 +2,7 @@ from zope.component import adapter
 from zope.component import queryUtility
 
 from zope.lifecycleevent.interfaces import IObjectCreatedEvent
+from zope.lifecycleevent.interfaces import IObjectCopiedEvent
 
 from plone.uuid.interfaces import IUUIDGenerator
 from plone.uuid.interfaces import IAttributeUUID
@@ -11,7 +12,11 @@ from plone.uuid.interfaces import ATTRIBUTE_NAME
 
 @adapter(IAttributeUUID, IObjectCreatedEvent)
 def addAttributeUUID(obj, event):
-
+    
+    if not IObjectCopiedEvent.providedBy(event):
+        if getattr(obj, ATTRIBUTE_NAME, None):
+            return  # defensive: keep existing UUID on non-copy create
+    
     generator = queryUtility(IUUIDGenerator)
     if generator is None:
         return
